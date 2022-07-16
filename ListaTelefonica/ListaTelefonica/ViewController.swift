21e4d18e17ef51737c32ac9a719e5f485897a6cc
Rosemere Moreira Tocafundo <rosemeremt@ciandt.com>
2022-07-16 14:23:21 -0300

    Criado a primeira tela do desafio lista telefonica

---
+++ ListaTelefonica/ListaTelefonica/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ListaTelefonica
+//
+//  Created by Rosemere Moreira Tocafundo on 14/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func adicionar(_ sender: Any) {
+    
+    print("nomes da lista")
+    }
+}
+
